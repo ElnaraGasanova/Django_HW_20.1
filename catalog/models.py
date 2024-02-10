d449d2466cb5ba8fe33d73807e3c47af5bac0bfa
ElnaraGasanova <elnaraskypro@gmail.com>
2024-02-11 00:23:09 +0300

    Формирование моделей.

--- catalog/models.py
+++ catalog/models.py
@@ -4,12 +4,35 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('name',)
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    image = models.ImageField(upload_to='images/products/', verbose_name='Изображение (превью)', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    created_at = models.DateTimeField(verbose_name='Дата создания (записи в БД)', auto_now_add=True, **NULLABLE)
+    updated_at = models.DateTimeField(verbose_name='Дата последнего изменения (записи в БД)', auto_now=True, **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} цена: {self.price}, категория: {self.category}'
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукт"
+        ordering = ("name",)
 
 
 categ_1 = Category.objects.create(name='Овощи и Фрукты')
