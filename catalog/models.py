04618585f889fdb8ee1079ddb4094c3e12e19137
ElnaraGasanova <elnaraskypro@gmail.com>
2024-02-11 01:25:41 +0300

    Пока не запускается, выдает ошибку.

--- catalog/models.py
+++ catalog/models.py
@@ -34,12 +34,12 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-
-categ_1 = Category.objects.create(name='Овощи и Фрукты')
-categ_2 = Category.objects.create(name='Бакалея')
-categ_3 = Category.objects.create(name='Хозтовары')
-categ_4 = Category.objects.create(name='Одежда')
-prduct_1 = Product.objects.create(name='Ананас', category=categ_1)
-prduct_2 = Product.objects.create(name='Кофе', category=categ_2)
-prduct_3 = Product.objects.create(name='Пакеты для мусора', category=categ_3)
-prduct_4 = Product.objects.create(name='Пижама', category=categ_4)
+#
+# categ_1 = Category.objects.create(name='Овощи и Фрукты')
+# categ_2 = Category.objects.create(name='Бакалея')
+# categ_3 = Category.objects.create(name='Хозтовары')
+# categ_4 = Category.objects.create(name='Одежда')
+# prduct_1 = Product.objects.create(name='Ананас', category=categ_1)
+# prduct_2 = Product.objects.create(name='Кофе', category=categ_2)
+# prduct_3 = Product.objects.create(name='Пакеты для мусора', category=categ_3)
+# prduct_4 = Product.objects.create(name='Пижама', category=categ_4)
