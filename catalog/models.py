82121312a628fee4bff5d907ce1064c81da80dd2
ElnaraGasanova <elnaraskypro@gmail.com>
2024-02-11 01:00:13 +0300

    Формирование моделей.

--- catalog/models.py
+++ catalog/models.py
@@ -7,32 +7,32 @@ class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание', **NULLABLE)
 
-    def __str__(self):
-        return f'{self.name}'
-
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
-        ordering = ('name',)
+        ordering = ['name',]
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание', **NULLABLE)
-    image = models.ImageField(upload_to='images/products/', verbose_name='Изображение (превью)', **NULLABLE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    image = models.ImageField(upload_to='prod_images', verbose_name='Изображение (превью)', **NULLABLE)
+    category = models.ForeignKey(Category, related_name='products', verbose_name='Категория',
+                                 on_delete=models.SET_NULL, **NULLABLE)
     price = models.IntegerField(verbose_name='Цена за покупку')
-    created_at = models.DateTimeField(verbose_name='Дата создания (записи в БД)', auto_now_add=True, **NULLABLE)
-    updated_at = models.DateTimeField(verbose_name='Дата последнего изменения (записи в БД)', auto_now=True, **NULLABLE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.name} цена: {self.price}, категория: {self.category}'
+    created_at = models.DateField(verbose_name='Дата создания (записи в БД)', auto_now_add=True, **NULLABLE)
+    updated_at = models.DateField(verbose_name='Дата последнего изменения (записи в БД)', auto_now=True, **NULLABLE)
 
     class Meta:
         verbose_name = "Продукт"
-        verbose_name_plural = "Продукт"
-        ordering = ("name",)
+        verbose_name_plural = "Продукты"
+        ordering = ["name",]
+
+    def __str__(self):
+        return self.name
 
 
 categ_1 = Category.objects.create(name='Овощи и Фрукты')
