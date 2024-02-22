a9e7b83f271c7dfe628a4b856c7bb2e926ba9474
ElnaraGasanova <elnaraskypro@gmail.com>
2024-02-23 00:53:57 +0300

    Настройки урлов и вью файла.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from catalog.views import home, contacts
+from catalog.views import home, product_info
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+from catalog.views import home, product_info
+from catalog.apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('', contacts)
+    path('', home, name='home'),
+    path('products/<int:pk>/', product_info, name='product_info'),
+    # path('', contacts)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 
 def home(request):
-    '''Выводит на страницы трех питомцев'''
+    '''Выводит на страницу все товары'''
     products = Product.objects.all()
     context = {
         'object_list': products
     }
     return render(request, 'catalog/home.html', context)
 
-def contacts(request):
-    return render(request, 'catalog/contacts.html')
+def product_info(request, pk):
+    '''Выводит на страницу товар по pk.'''
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'object': product
+    }
+    return render(request, 'dogs/product_info.html', context)
+
+
+# def contacts(request):
+#     return render(request, 'catalog/contacts.html')
 
