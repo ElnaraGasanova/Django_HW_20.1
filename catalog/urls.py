c6ff0a1f0bab1ff09c1cb653fe8e20ac489d5b19
ElnaraGasanova <elnaraskypro@gmail.com>
2024-02-29 14:42:34 +0300

    path ListView, DetailView, CreateView, UpdateView

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from catalog.views import HomeView, ProductDetailView, contacts
+from catalog.views import HomeView, ProductDetailView, contacts, ProductCreateView, ProductUpdateView
 from django.conf.urls.static import static
 from django.conf import settings
 from catalog.apps import CatalogConfig
@@ -9,5 +9,9 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_info'),
-    path('contacts/', contacts, name='w')
+    path('contacts/', contacts, name='w'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#     path('contacts/', ContactsPageView.as_view(), name='w')
