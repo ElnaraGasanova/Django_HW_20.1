e7bb49fb5a98cc72aac6af2a340664971fde8c25
ElnaraGasanova <elnaraskypro@gmail.com>
2024-02-29 12:36:12 +0300

    Переведены контроллеры с FBV на CBV.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path
-from catalog.views import home, product_info, contacts
-
+from catalog.views import HomeView, ProductDetailView, contacts
 from django.conf.urls.static import static
 from django.conf import settings
-
-from catalog.views import home, product_info
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('products/<int:pk>/', product_info, name='product_info'),
+    path('', HomeView.as_view(), name='home'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_info'),
     path('contacts/', contacts, name='w')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+
 from catalog.models import Product
 
-def home(request):
-    '''Выводит на страницу все товары'''
-    products = Product.objects.all()
-    context = {
-        'object_list': products
-    }
-    return render(request, 'catalog/product_list.html', context)
 
-def product_info(request, pk):
-    '''Выводит на страницу товар по pk.'''
-    product = get_object_or_404(Product, pk=pk)
-    context = {
-        'object': product
-    }
-    return render(request, 'catalog/product_info.html', context)
+# Контроллер CBV
+class HomeView(ListView):
+    model = Product
+
+# Контроллер CBV
+class ProductDetailView(DetailView):
+    model = Product
 
 
+# Контроллер FBV
 def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -29,3 +25,21 @@ def contacts(request):
     }
     return render(request, 'catalog/contacts.html', context)
 
+# Контроллер FBV
+# def home(request):
+#     '''Выводит на страницу все товары'''
+#     products = Product.objects.all()
+#     context = {
+#         'object_list': products
+#     }
+#     return render(request, 'catalog/product_list.html', context)
+
+
+# Контроллер FBV
+# def product_info(request, pk):
+#     '''Выводит на страницу товар по pk.'''
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {
+#         'object': product
+#     }
+#     return render(request, 'catalog/product_detail.html', context)
