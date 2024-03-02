c0154941f0b2578d2d6c116883114b266af8b061
ElnaraGasanova <elnaraskypro@gmail.com>
2024-03-02 12:23:28 +0300

    Прописаны докстринги

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,6 +4,7 @@ from catalog.models import Category, Product, Blog
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    '''Класс для регистрации категории в админке.'''
     list_display = ("id", "name",)
     list_filter = ("name",)
     search_fields = ("name", "description",)
@@ -11,6 +12,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    '''Класс для регистрации продукта в админке.'''
     list_display = ("id", "name", "price", "category",)
     list_filter = ("category",)
     search_fields = ("name", "description",)
@@ -18,6 +20,7 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
+    '''Класс для регистрации публикации в админке.'''
     list_display = ("id", "title", "is_published", "slug",)
     list_filter = ("title",)
     search_fields = ("title", "is_published",)
