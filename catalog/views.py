89ac9b6ef2ef1ab3e470527a29b747cda0d8a55c
ElnaraGasanova <elnaraskypro@gmail.com>
2024-03-02 12:29:56 +0300

    Выполнена настройка вывода только активных публикаций.

--- catalog/views.py
+++ catalog/views.py
@@ -31,6 +31,11 @@ def contacts(request):
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
 
 class BlogDetailView(DetailView):
     model = Blog
